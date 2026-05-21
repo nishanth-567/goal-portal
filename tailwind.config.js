79c0a4842e51c04fb0115bb1639292f864fc0cf5
Nishanth Mandava <mandavanishanth@gmail.com>
2026-05-21 10:41:33 +0530

    Simplify Tailwind theme configuration
    Removed custom theme colors from Tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      borderColor: { border: "hsl(var(--border))" },
-      backgroundColor: { background: "hsl(var(--background))" },
-      textColor: { foreground: "hsl(var(--foreground))" },
-    },
+    extend: {},
   },
   plugins: [require("tailwindcss-animate")],
 };
