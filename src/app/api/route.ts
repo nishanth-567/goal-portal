e7df0a169cfe8c3e3907b533827143c4e9b37304
Nishanth Mandava <mandavanishanth@gmail.com>
2026-05-21 09:14:14 +0530

    Delete src/app/api/route.ts

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-
-export async function GET() {
-  const hash = await bcrypt.hash("demo123", 10);
-  const check = await bcrypt.compare("demo123", hash);
-  return NextResponse.json({ hash, check });
-}
